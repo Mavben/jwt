c7f5dc284c7f14e4e1931d02a0294e73a78d1a8d
Mavben <evrok907@gmail.com>
2024-03-24 13:10:22 +0900

    Feat: Add Signup Logic
    회원가입 로직 구현: JoinDTO, JoinControlelr, JoinService

---
+++ src/main/java/com/example/jwtpractice/service/JoinService.java
@@ -0,0 +1,37 @@
+package com.example.jwtpractice.service;
+
+import com.example.jwtpractice.dto.JoinDTO;
+import com.example.jwtpractice.entity.UserEntity;
+import com.example.jwtpractice.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class JoinService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByUsername(username);
+
+        if (isExist) {
+            return;
+        }
+
+        UserEntity data = new UserEntity();
+
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_ADMIN");
+
+        userRepository.save(data);
+    }
+}
